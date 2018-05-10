af0f2c31b5ca5ba8481625a4b24eb2391824c96d
Julian Simioni <julian@simioni.org>
2018-05-10 16:40:00 -0400

    Install Node

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -2,11 +2,18 @@ git clone https://github.com/pelias/dockerfiles.git
 
 cp pelias.json dockerfiles/pelias.json
 
+# run full build with dockerfiles, this also starts services
 cd dockerfiles
+ls
 ./build.sh
-
-
 cd ..
+
+# install node/npm for tests
+NODE_VERSION='6.14.0'
+git clone 'https://github.com/isaacs/nave.git'
+./nave/nave.sh 'usemain' "${NODE_VERSION}"
+
+# run tests
 git clone https://github.com/pelias/acceptance-tests.git
 cd acceptance-tests
 npm i
