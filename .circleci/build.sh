4167907ff1552ccfb32e4bb262aa0a4faa628d03
Julian Simioni <julian@simioni.org>
2018-05-10 16:37:00 -0400

    Full build for portland

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -1,1 +1,13 @@
 git clone https://github.com/pelias/dockerfiles.git
+
+cp pelias.json dockerfiles/pelias.json
+
+cd dockerfiles
+./build.sh
+
+
+cd ..
+git clone https://github.com/pelias/acceptance-tests.git
+cd acceptance-tests
+npm i
+npm test -- -e local test_cases/portland/*.json
