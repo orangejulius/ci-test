389f4e021be96c956e37026d2b228d1b3bd01bfd
Julian Simioni <julian@simioni.org>
2018-05-11 14:43:57 -0400

    Download WOF data with a progress bar
    Without this, the build hang protection of CircleCI takes effect after
    10 mins

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -18,6 +18,13 @@ git clone https://github.com/pelias/dockerfiles.git -b circle
 
 cp pelias.json dockerfiles/pelias.json
 
+# download wof data separately, with a progress meter, so the build does not expire due to the length of time it takes
+mkdir -p /tmp/whosonfirst/sqlite
+pushd /tmp/whosonfirst/sqlite > /dev/null
+echo @`date +%s` > whosonfirst-data-latest.db.timestamp
+curl https://dist.whosonfirst.org/sqlite/whosonfirst-data-latest.db.bz2 | pv | bunzip2 > whosonfirst-data-latest.db
+popd > /dev/null
+
 # run full build with dockerfiles, this also starts services
 cd dockerfiles
 docker-compose ps
