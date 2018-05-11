f3aa4bd8b87aee69adbf447ba5866627a60bfed8
Julian Simioni <julian@simioni.org>
2018-05-11 19:34:54 -0400

    Test

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -euo pipefail
 
+pwd
+exit 0
+
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
