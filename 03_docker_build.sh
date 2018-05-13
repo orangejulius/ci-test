3634f26acb6820ba8db3b8241524372867ff2075
Julian Simioni <julian@simioni.org>
2018-05-13 11:30:55 -0400

    Run docker-compose pull as a separate step

--- 03_docker_build.sh
+++ 03_docker_build.sh
@@ -2,10 +2,8 @@
 set -euo pipefail
 
 # run full build with dockerfiles, this also starts services
-cp docker-compose.yml dockerfiles/
 cd dockerfiles
 bash ./build.sh
-cd ..
 
 # give libpostal some time to boot
 sleep 30
