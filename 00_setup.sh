81da19c503cfe5ccdb37070773df1efa6add8e55
Julian Simioni <julian@simioni.org>
2018-05-19 18:06:51 -0400

    Install docker and docker-compose properly

--- 00_setup.sh
+++ 00_setup.sh
@@ -8,10 +8,17 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
 sudo apt update
 sudo apt install -y docker-ce curl git pv
 
+# install docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo chmod ugo+x /usr/local/bin/docker-compose
+
+# start docker
+sudo /etc/init.d/docker start
+
 # install node/npm for tests
 NODE_VERSION='6.14.0'
 git clone 'https://github.com/isaacs/nave.git'
-bash nave/nave.sh 'usemain' "${NODE_VERSION}"
+sudo bash nave/nave.sh 'usemain' "${NODE_VERSION}"
 
 #install dockerfiles
 git clone https://github.com/pelias/dockerfiles.git -b circle
