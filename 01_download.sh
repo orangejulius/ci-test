fbaae94979cce85b211caca835594392e644d26f
Julian Simioni <julian@simioni.org>
2018-05-28 14:02:24 -0400

    Provide several download options for WOF data
    Looks like uncompressed download is fastest within AWS

--- 01_download.sh
+++ 01_download.sh
@@ -8,10 +8,15 @@ cd $DATA_DIR/dockerfiles
 docker-compose pull &
 
 # download wof data separately, with a progress meter, so the build does not expire due to the length of time it takes
+# if it happens to run on CircleCI. CircleCI kills builds that do not output anything at least once per 10 minutes
 mkdir -p $DATA_DIR/whosonfirst/sqlite
 cd $DATA_DIR/whosonfirst/sqlite
 echo @`date +%s` > whosonfirst-data-latest.db.timestamp
-curl https://dist.whosonfirst.org/sqlite/whosonfirst-data-latest.db.bz2 | pv | bunzip2 > whosonfirst-data-latest.db
+
+## provide diferent download options. on a very fast connection, downloading the uncompressed file is fastest
+#curl https://dist.whosonfirst.org/sqlite/whosonfirst-data-latest.db.bz2 | pv | bunzip2 > whosonfirst-data-latest.db
+#curl https://s3.amazonaws.com/pelias-data.nextzen.org/whosonfirst/whosonfirst-data-latest.db.gz | pv | gunzip > whosonfirst-data-latest.db
+curl https://s3.amazonaws.com/pelias-data.nextzen.org/whosonfirst/whosonfirst-data-latest.db | pv > whosonfirst-data-latest.db
 
 # also download other custom data
 cd $DATA_DIR/openaddresses
