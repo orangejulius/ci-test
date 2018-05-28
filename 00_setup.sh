f00de0e27b46f745364de96a3a49e187710c616d
Julian Simioni <julian@simioni.org>
2018-05-28 12:14:06 -0400

    Remove setup steps moved to AMI
    Now that we are abandoning the idea of running this in CircleCI, things
    can get simpler

--- 00_setup.sh
+++ 00_setup.sh
@@ -1,27 +1,6 @@
 #!/bin/bash
 set -euo pipefail
 
-# set up required ubuntu dependencies
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-
-sudo apt update
-sudo apt install -y docker-ce curl git pv
-
-# install docker-compose
-sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
-sudo chmod ugo+x /usr/local/bin/docker-compose
-
-# start docker
-sudo /etc/init.d/docker start || true
-sudo gpasswd -a $USER docker
-newgrp docker
-
-# install node/npm for tests
-NODE_VERSION='6.14.0'
-git clone 'https://github.com/isaacs/nave.git'
-sudo bash nave/nave.sh 'usemain' "${NODE_VERSION}"
-
 #install dockerfiles
 git clone https://github.com/pelias/dockerfiles.git -b circle
 
